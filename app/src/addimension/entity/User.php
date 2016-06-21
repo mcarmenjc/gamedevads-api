4c4c7b19461097f406f954117728a0be6b4db733
Piero Buttazzo <piero.buttazzo@affiliatewindow.com>
2016-06-21 12:28:50 +0100

    generated the getter and setter

--- app/src/addimension/entity/User.php
+++ app/src/addimension/entity/User.php
@@ -1,32 +1,113 @@
 <?php
+
 namespace App\Addimension\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
 /**
- * @ORM\Entity
- * @ORM\Table(name="user")
+ * User
  */
 class User
 {
     /**
-     * @ORM\Column(type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var integer
      */
     private $id;
+
     /**
-    * @ORM\Column(type="string", length=100)
-    */
+     * @var string
+     */
     private $name;
+
     /**
-    * @ORM\Column(type="string", length=100)
-    */
+     * @var string
+     */
     private $email;
+
     /**
-    * @ORM\Column(type="integer")
-    */
+     * @var integer
+     */
     private $type;
 
 
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     *
+     * @return User
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return integer
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 }
+
